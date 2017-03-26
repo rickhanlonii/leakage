819fdd16859c03d3a1193b4e3e73513ed6a38dcc
Andy Wermke <andy@dev.next-step-software.com>
2017-03-26 19:43:38 +0200

    Make test code a little friendlier

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,6 +1,8 @@
 /* eslint-env mocha */
 // Using mocha, since for memory leak testing we want the tests run serially, anyway
 
+require('chai').use(require('chai-as-promised'))
+
 const assert = require('chai').assert
 const expect = require('chai').expect
 const iterate = require('../lib').iterate
@@ -35,28 +37,15 @@ describe('leakage', () => {
   it('returns a rejecting promise when testing leaky code', () => {
     const objects = []
 
-    const promise = iterate(100, () => new Promise(
-      resolve => setTimeout(() => {
-        const newObject = { foo: 'bar' }
-        objects.push(newObject)     // <= leak
-        resolve()
-      }, 20)
-    ))
-    assert(promise.then)
-    expect(typeof promise.then).to.equal('function')
-
-    return new Promise((resolve, reject) => {
-      promise.then(
-        () => reject(new Error(`Promise should not succeed.`)),
-        (error) => {
-          if (error.message.match(/^Heap grew on \d subsequent garbage collections[\s\S]*Iterations between GCs: ~17[\s\S]*Final GC details:/)) {
-            resolve()
-          } else {
-            reject(new Error(`Unexpected promise rejection: ${error.message}`))
-          }
-        }
-      )
-    })
+    return expect(
+      iterate(100, () => new Promise(
+        resolve => setTimeout(() => {
+          const newObject = { foo: 'bar' }
+          objects.push(newObject)     // <= leak
+          resolve()
+        }, 20)
+      ))
+    ).to.eventually.be.rejectedWith(/^Heap grew on \d subsequent garbage collections[\s\S]*Iterations between GCs: ~17[\s\S]*Final GC details:/)
   })
 
   it('returns a resolving promise when testing non-leaky code', () => {
